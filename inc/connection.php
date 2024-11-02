2d725e2dcea784f99099ec60cf2f9a9dc1dcacf4
Zikolee <114690048+Zikolee@users.noreply.github.com>
2024-11-02 17:18:47 +0100

    Create connection.php

---
+++ inc/connection.php
@@ -0,0 +1,11 @@
+<?php
+$host = "localhost";
+$database = "agriway";
+$user = "root";
+$password = "";
+//connecting to mysql database
+$connection = mysqli_connect($host, $user, $password, $database) or die("Database can not connect");
+
+if($connection){
+    
+}
